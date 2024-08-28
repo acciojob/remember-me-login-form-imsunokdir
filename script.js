bf0181d7903c831400e4d4db304b410f4b3731b5
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-08-28 14:48:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+
+const loginForm = document.getElementById("login-form");
+
+document.addEventListener('DOMContentLoaded',()=>{
+	const uName = localStorage.getItem("username");
+	const pWord = localStorage.getItem("password");
+
+	if(uName && pWord){
+		const exist = document.getElementById("existing")
+		exist.style.display="block"
+		exist.addEventListener('click',()=>{
+		alert(`Logged in as ${uName}`) 
+	})
+	}
+
+	loginForm.addEventListener("submit",(event)=>{
+	event.preventDefault();
+	const username = document.getElementById("username").value;
+	const password = document.getElementById("password").value;
+	const remember = document.getElementById("checkbox").checked;
+
+	if(remember){
+		localStorage.setItem('username',username);
+		localStorage.setItem('password', password);
+	}else{
+		localStorage.removeItem("username"); 
+		localStorage.removeItem("password");
+	}
+	alert(`Logged in as ${username}`)
+	// document.getElementById("existing").style.display = "block"
+
+	})
+	
+	
+})
+
+
+
+
+
+
+
+
+
